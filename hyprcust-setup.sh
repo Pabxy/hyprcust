c0875ba4ed69f282cd3697949042c6e070ee13d2
Pabxy <pabxy2020@gmail.com>
2022-12-01 11:30:25 +0100

    modified:   hyprcust-setup.sh

--- hyprcust-setup.sh
+++ hyprcust-setup.sh
@@ -1,21 +1,24 @@
 #Hyprland Custom Script#
 
-    sudo pacman -S zsh pulseaudio pavucontrol nano git neofetch --noconfirm
+
+echo "Instalando dependencias....."
+    sudo pacman -S zsh pulseaudio pavucontrol nano git neofetch --noconfirm > /dev/null 2>&1
+
 echo "Instalando todos los paquetes....."    
-    sudo pacman -U ./paquetes/* --noconfirm
+    sudo pacman -U ./paquetes/* --noconfirm > /dev/null 2>&1
 
 echo "Instalando programas basicos adicionales"
-        paru -S visual-studio-code-bin --noconfirm
+        paru -S visual-studio-code-bin --noconfirm > /dev/null 2>&1
     echo "Visual Studio Code --> OK"
         
-         paru -S google-chrome --noconfirm
+         paru -S google-chrome --noconfirm > /dev/null 2>&1
     echo "Google Chrome --> OK"
     
-        paru -S debtap --noconfirm
+        paru -S debtap --noconfirm > /dev/null 2>&1
     echo "Debtap --> OK"
 
-        paru -S virtualbox-host-modules-arch --noconfirm
-        paru -S virtualbox --noconfirm
+        paru -S virtualbox-host-modules-arch --noconfirm > /dev/null 2>&1
+        paru -S virtualbox --noconfirm > /dev/null 2>&1
             sudo modprobe vboxdrv
             sudo modprobe vboxnetadp
             sudo modprobe vboxnetflt
@@ -25,5 +28,5 @@ echo "Instalando programas basicos adicionales"
 echo ""
 
 echo "Configurando paquetes...."
-    cp -r ./config/* ~/.config
-    sudo chown -R pablo:pablo ~/.config
+    cp -r ./config/* ~/.config > /dev/null 2>&1
+    sudo chown -R pablo:pablo ~/.config > /dev/null 2>&1
