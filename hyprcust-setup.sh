3046cd37d6f5eb75dc7389ff2e2ce27cc2f16067
Pabxy <pabxy2020@gmail.com>
2022-12-01 11:41:06 +0100

    modified:   hyprcust-setup.sh

--- hyprcust-setup.sh
+++ hyprcust-setup.sh
@@ -1,10 +1,10 @@
 #Hyprland Custom Script#
 
 
-echo "Instalando dependencias....."
+echo "-> Instalando dependencias....."
     sudo pacman -S zsh pulseaudio pavucontrol nano git neofetch kitty --noconfirm > /dev/null 2>&1
 
-echo "Instalando todos los paquetes....."    
+echo "-> Instalando todos los paquetes....."    
     sudo pacman -U ./paquetes/* --noconfirm > /dev/null 2>&1
     echo """
     Hyprland    OK
@@ -21,33 +21,33 @@ echo "Instalando todos los paquetes....."
     sleep 3
     echo ""
 
-echo "Instalando programas basicos adicionales"
+echo "-> Instalando programas basicos adicionales"
 echo ""
         paru -S visual-studio-code-bin --noconfirm > /dev/null 2>&1
-    echo "  Visual Studio Code  OK"
+    echo "      Visual Studio Code  OK"
         
          paru -S google-chrome --noconfirm > /dev/null 2>&1
-    echo "  Google Chrome       OK"
+    echo "      Google Chrome       OK"
     
         paru -S debtap --noconfirm > /dev/null 2>&1
-    echo "  Debtap              OK"
+    echo "      Debtap              OK"
 
         paru -S virtualbox-host-modules-arch --noconfirm > /dev/null 2>&1
         paru -S virtualbox --noconfirm > /dev/null 2>&1
             sudo modprobe vboxdrv
             sudo modprobe vboxnetadp
             sudo modprobe vboxnetflt
-    echo "VirtualBox --> OK"
+    echo "      VirtualBox          OK"
 
 
 echo ""
 
-echo "Configurando paquetes...."
+echo "-> Configurando paquetes...."
 echo ""
     cp -r ./config/* ~/.config > /dev/null 2>&1
     sudo chown -R pablo:pablo ~/.config > /dev/null 2>&1
 
-echo "HYPRCUST SETUP COMPLETE"
+echo "      HYPRCUST SETUP COMPLETE"
 echo ""
 sleep 3
 exit
