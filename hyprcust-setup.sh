7d29b012615b4f0348a56528cfeb7567f1d6485d
Pabxy <pabxy2020@gmail.com>
2022-12-01 11:12:27 +0100

    new file:   config/hypr/hyprland.conf 	new file:   config/hypr/hyprpaper.conf 	new file:   config/hypr/wallpapers/main.jpg 	new file:   config/kitty/kitty.conf 	new file:   config/neofetch/config.conf 	new file:   config/waybar/config 	new file:   config/waybar/style.css 	modified:   hyprcust-setup.sh 	new file:   paquetes/agave-nerd-font.pacman 	new file:   paquetes/sddm.pacman

--- hyprcust-setup.sh
+++ hyprcust-setup.sh
@@ -1,1 +1,11 @@
 #Hyprland Custom Script#
+
+    sudo pacman -S zsh pulseaudio pavucontrol nano git --noconfirm
+echo "Instalando todos los paquetes....."    
+    sudo pacman -U ./paquetes/* --noconfirm
+
+echo ""
+
+echo "Configurando paquetes...."
+    cp -r ./config/* ~/.config
+    sudo chown -R pablo:pablo ~/.config
