9e3151630fcf459ccd49930ea6ee9fca8d3f6461
Pabxy <pabxy2020@gmail.com>
2022-12-01 11:24:26 +0100

    modified:   hyprcust-setup.sh 	new file:   paquetes/google-chrome.pacman

--- hyprcust-setup.sh
+++ hyprcust-setup.sh
@@ -1,8 +1,21 @@
 #Hyprland Custom Script#
 
-    sudo pacman -S zsh pulseaudio pavucontrol nano git --noconfirm
+    sudo pacman -S zsh pulseaudio pavucontrol nano git neofetch --noconfirm
 echo "Instalando todos los paquetes....."    
     sudo pacman -U ./paquetes/* --noconfirm
+
+echo "Instalando programas basicos adicionales"
+        paru -S visual-studio-code-bin --noconfirm
+    echo "Visual Studio Code --> OK"
+        
+    echo "Debtap --> OK"
+        paru -S virtualbox-host-modules-arch --noconfirm
+        paru -S virtualbox --noconfirm
+            sudo modprobe vboxdrv
+            sudo modprobe vboxnetadp
+            sudo modprobe vboxnetflt
+    echo "VirtualBox --> OK"
+
 
 echo ""
 
