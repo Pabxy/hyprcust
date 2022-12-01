f2bfbdbaacc8b4b09c2d55ee2667be5e606b58da
Pabxy <pabxy2020@gmail.com>
2022-12-01 11:36:29 +0100

    modified:   hyprcust-setup.sh

--- hyprcust-setup.sh
+++ hyprcust-setup.sh
@@ -2,10 +2,24 @@
 
 
 echo "Instalando dependencias....."
-    sudo pacman -S zsh pulseaudio pavucontrol nano git neofetch --noconfirm > /dev/null 2>&1
+    sudo pacman -S zsh pulseaudio pavucontrol nano git neofetch kitty --noconfirm > /dev/null 2>&1
 
 echo "Instalando todos los paquetes....."    
     sudo pacman -U ./paquetes/* --noconfirm > /dev/null 2>&1
+    echo """
+    Hyprland    OK
+    Hyprpaper   OK
+    Waybar      OK
+    Paru        OK
+    Kitty       OK
+    Sddm        OK
+    Rofi        OK
+    Office-356  OK
+    Oh-My-Zsh   OK
+    Agave Font  OK
+    """
+    sleep 3
+    echo ""
 
 echo "Instalando programas basicos adicionales"
         paru -S visual-studio-code-bin --noconfirm > /dev/null 2>&1
