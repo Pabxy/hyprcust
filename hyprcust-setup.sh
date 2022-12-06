7d637ac873d7c7b0585473895c9f88beb5ed6e59
Pabxy <pabxy2020@gmail.com>
2022-12-06 19:02:40 +0100

    modified:   hyprcust-setup.sh 	deleted:    paquetes/office-365.pacman

--- hyprcust-setup.sh
+++ hyprcust-setup.sh
@@ -7,26 +7,44 @@ echo "-> Instalando dependencias....."
     sudo pacman -S zsh pulseaudio pavucontrol nano git neofetch kitty --noconfirm > /dev/null 2>&1
 
 echo "-> Instalando todos los paquetes....."    
-    sudo pacman -U ./paquetes/* --noconfirm > /dev/null 2>&1
-    echo """
-    Hyprland    OK
-    Hyprpaper   OK
-    Waybar      OK
-    Paru        OK
-    Kitty       OK
-    Sddm        OK
-    Rofi        OK
-    Office-356  OK
-    Oh-My-Zsh   OK
-    Agave Font  OK
-    """
-    sleep 3
-    echo ""
+
+    sudo pacman -U ./paquetes/hyprland.pacman --noconfirm > /dev/null 2>&1
+    echo "  Hyprland ->         OK"
+
+    sudo pacman -U ./paquetes/hyprpaper.pacman --noconfirm > /dev/null 2>&1
+    echo "  Hyprpaper ->        OK"
+
+    sudo pacman -U ./paquetes/waybar.pacman --noconfirm > /dev/null 2>&1
+    echo "  Waybar ->           OK"
+
+    sudo pacman -U ./paquetes/paru.pacman --noconfirm > /dev/null 2>&1
+    echo "  Paru ->             OK"
+
+    sudo pacman -U ./paquetes/sddm.pacman --noconfirm > /dev/null 2>&1
+    echo "  SDDM ->             OK"
+
+    sudo pacman -U ./paquetes/rofi.pacman --noconfirm > /dev/null 2>&1
+    echo "  Rofi ->             OK"
+    
+    sudo pacman -U ./paquetes/sddm.pacman --noconfirm > /dev/null 2>&1
+    echo "  SDDM ->             OK"
+    
+    sudo pacman -U ./paquetes/oh-my-zsh.pacman --noconfirm > /dev/null 2>&1
+    sudo pacman -U ./paquetes/zsh-autosuggestions.pacman --noconfirm > /dev/null 2>&1
+    sudo pacman -U ./paquetes/zsh-syntax-highlighting.pacman --noconfirm > /dev/null 2>&1
+    echo "  OH-MY-ZSH ->        OK"
+
+    sudo pacman -U ./paquetes/oh-my-zsh.pacman --noconfirm > /dev/null 2>&1
+    echo "  Agave Nerd Font ->  OK"
+
 
 echo "-> Instalando programas basicos adicionales"
 echo ""
         paru -S visual-studio-code-bin --noconfirm > /dev/null 2>&1
     echo "      Visual Studio Code  OK"
+
+        paru -S ms-365-electron --noconfirm > /dev/null 2>&1
+    echo "      Office 365          OK"
         
          paru -S google-chrome --noconfirm > /dev/null 2>&1
     echo "      Google Chrome       OK"
@@ -52,6 +70,7 @@ echo ""
     chsh -s /bin/zsh
     sudo systemctl enable sddm
 
+echo ""
 echo "      HYPRCUST SETUP COMPLETE"
 echo ""
 sleep 3
